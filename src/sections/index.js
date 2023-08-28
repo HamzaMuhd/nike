72a4dded83bd7b486c72a043a2b633b0bf783dad
HamzaMuhd <hamzamuhdidris@gmail.com>
2023-08-28 23:42:47 +0100

    nike nav

---
+++ src/sections/index.js
@@ -0,0 +1,22 @@
+import Hero from "./Hero";
+import Footer from "./Footer";
+import CustomerReviews from "./CustomerReviews";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+
+
+
+
+export {
+    Hero,
+    Footer,
+    CustomerReviews,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality,
+    PopularProducts,
+    Services
+}
