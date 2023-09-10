8a21b9a953cffc8185b7865586a25cd7ad384f3c
HamzaMuhd <hamzamuhdidris@gmail.com>
2023-09-10 12:25:31 +0100

    completed nike landing page

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,11 @@
-const Button = ({label, iconUrl, backgroundColor, borderColor, textColor}) => {
+const Button = ({label, iconUrl, backgroundColor, borderColor, textColor, fullWidth}) => {
   return (
    <button className={`flex justify-center items-center gap-2 px-7 
    py-4 border font-montserrat text-lg leading-none
    ${backgroundColor ? `${backgroundColor} ${textColor}
    ${borderColor}` 
   :  'bg-coral-red text-white border-coral-red'
-  } rounded-full`}>
+  } rounded-full ${fullWidth && "w-full"}`}>
     {label}
     {iconUrl && 
     <img src={iconUrl} alt='arrowRight'
